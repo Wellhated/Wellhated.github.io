63c1c00747b5fd93cad10afe9b8c1f59bb4b42b0
Landrei Zerna <zernalandrei@gmail.com>
2022-08-13 04:01:28 +0800

    Added Student View

--- js/script-new.js
+++ js/script-new.js
@@ -9,8 +9,16 @@ $(document).ready(() => {
 
     // view-record.html only
     $(".records .dataTable-wrapper .dataTable-top .dataTable-dropdown").html("");
+    
+    // view-faculty.html only
     if(window.matchMedia("(max-width: 767px)").matches){
         // $(".records .dataTable-wrapper .dataTable-top .dataTable-search .dataTable-input").css("width", "17.5rem");
+        $(".faculty .dataTable-wrapper .dataTable-top .dataTable-dropdown").html("");
     }
+
+    // E-Class Record
+    $(".e-class-event tbody tr").on("click", (e) => {
+        console.log((e.currentTarget.id).slice(3));
+    });
 })
 
